e128329d6d5ac6c75ce93ad7e42d6951ce4870e8
zrtheya <zrtheyatuares@yahoo.com>
2019-03-10 17:40:36 +0800

    kekekkeke

--- app/src/main/java/com/example/frontend/RegisterActivity.java
+++ app/src/main/java/com/example/frontend/RegisterActivity.java
@@ -11,6 +11,9 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
     FirebaseAuth authentication;
@@ -30,38 +33,51 @@ public class RegisterActivity extends AppCompatActivity {
 
         signUpButton = findViewById(R.id.signUpButton);
 
-        signUpButton.setOnClickListener((View view)->{
+        signUpButton.setOnClickListener((View view) -> {
             signUp();
         });
 
     }
 
     private void signUp() {
-        String email = emailEditText.getText().toString();
-        String password = passwordEditText.getText().toString();
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
 
         int length = password.length();
-        if (email.equals("")){
-            Toast.makeText(this, "Email field is empty",Toast.LENGTH_LONG).show();
-        }else{
-            if (length < 8){
-                Toast.makeText(this, "Password must be not less than 8 characters",Toast.LENGTH_LONG).show();
-            } else {
-                authentication.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener((Task<AuthResult> task) -> {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(this, "success", Toast.LENGTH_LONG).show();
-                                Intent intent = new Intent(this, HomeActivity.class);
-                                startActivity(intent);
-                            } else {
-                                Toast.makeText(this, "Failure", Toast.LENGTH_LONG).show();
-                            }
-                        });
-            }
+        if (email.isEmpty()) {
+            emailEditText.setError("Email is required");
+            emailEditText.requestFocus();
+            return;
         }
 
+        if (password.isEmpty()) {
+            passwordEditText.setError("Password is required");
+            passwordEditText.requestFocus();
+            return;
+        }
 
+        if (length < 8) {
+            Toast.makeText(this, "Password must be not less than 8 characters", Toast.LENGTH_LONG).show();
+        } else {
+            authentication.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener((Task<AuthResult> task) -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(this, "success", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(this, HomeActivity.class);
+                            startActivity(intent);
+                        } else {
+
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                Toast.makeText(getApplicationContext(), "User already registered", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
 
     }
+
+
 
 }
